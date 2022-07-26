dc55bf9fa863ae498615adba04afec0b306733dd
Kristan Frost <kristan.frost@gmail.com>
2022-07-26 10:02:41 -0500

    Commit first demo day completed code.

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
 window.addEventListener('load', function() {
     event.preventDefault();
 
+    let form = document.getElementById("addBook");
+
+form.addEventListener("submit", function(event){
+
+    let title = document.querySelector("input[name=title]")
+    alert(`${title} was entered.`)
+
+
+
+
+})
+
 });
 
 
